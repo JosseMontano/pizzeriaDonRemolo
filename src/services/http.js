0fcbdea2544e852bc1262c5a75f3d1bb080daa7f
alefinir <31045393+alefinir@users.noreply.github.com>
2022-08-11 19:03:42 -0300

    react-create-app on Heroku

--- src/services/http.js
+++ src/services/http.js
@@ -1,5 +1,7 @@
 import axios from 'axios'; 
 
-const url="https://donremolo.site:8170/api/"; 
+//const url="https://donremolo.site:8170/api/"; 
+
+const url="https://donremolo-julio2022.herokuapp.com/api"; 
 
 export {axios, url}
