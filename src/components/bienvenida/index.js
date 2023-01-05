91c9b6f6f43d68fc66f376c454e0b56045023ae0
JosseMontano <josemariazambranamontano123@gmail.com>
2023-01-05 19:42:56 -0400

    replace tailwind by css in welcome-title

--- src/components/bienvenida/index.js
+++ src/components/bienvenida/index.js
@@ -54,7 +54,6 @@ const bienvenida = () => {
   return (
     <>
       <Titulo />
-      {/*<HeaderCarrito />*/}
       <Descripcion />
       <MostrarApi cats={cats} />
     </>
--- src/components/bienvenida/titulo.js
+++ src/components/bienvenida/titulo.js
@@ -2,6 +2,25 @@ import "../../styles/header/titulo.css";
 import "../../styles/App.css";
 import { useContext } from "react";
 import ModalContext from "./../../context/modalContext";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #fff;
+  /* height: 56px; */
+  height: 72px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 999;
+  box-shadow: 0 1px 10px rgba(0, 0, 0, 0.5);
+  p {
+    color: #e74423;
+    font-family: Cera-Pro-Medium;
+    font-size: 26px;
+    line-height: 1.75rem;
+    margin: 20px 20px;
+  }
+`;
 
 export default function Titulo() {
   const { modal, handleModal } = useContext(ModalContext);
@@ -9,17 +28,8 @@ export default function Titulo() {
     handleModal(modal);
   };
   return (
-    <>
-      <div
-        onClick={hideHandleModal}
-        className="bg-white shadow-header h-[56px] xl:h-[72px] xl:w-[100%] xl:fixed xl:top-[0px] xl:z-[999]"
-      >
-        <section className="flex items-start justify-start  sm:p-4 sm:flex sm:items-start sm:justify-start md:p-6 md:flex md:items-start md:justify-start lg:p-8  lg:flex lg:items-start lg:justify-start xl:p-5 xl:flex xl:items-start xl:justify-start 2xl:p-11 2xl:flex 2xl:items-start 2xl:justify-start">
-          <p className="absolute left-[12px] top-[15px] lg:ml-10 text-xl sm:text-titulo1280 md:text-titulo1280 lg:text-titulo1280 xl:text-titulo1280 xl:font-bold xl:leading-7 xl:block 2xl:8xl xl:tracking-wide xl:absolute xl:left-[100px] xl:top-[21px] desktop1920:absolute desktop1920:left-[300px]">
-            Pizzería Don Rémolo
-          </p>
-        </section>
-      </div>
-    </>
+    <Container onClick={hideHandleModal}>
+      <p>Pizzería Don Rémolo</p>
+    </Container>
   );
 }
